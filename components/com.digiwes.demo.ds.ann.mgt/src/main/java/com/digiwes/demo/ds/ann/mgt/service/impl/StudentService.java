afd3fb64779c5623ff0abaad21f15b53d2cbbb1c
liurl3 <liurl3@asiainfo.com>
2015-12-31 14:49:26 +0800

    modify ds annotation

--- components/com.digiwes.demo.ds.ann.mgt/src/main/java/com/digiwes/demo/ds/ann/mgt/service/impl/StudentService.java
+++ components/com.digiwes.demo.ds.ann.mgt/src/main/java/com/digiwes/demo/ds/ann/mgt/service/impl/StudentService.java
@@ -4,16 +4,16 @@ import com.digiwes.demo.ds.ann.mgt.bean.Student;
 import com.digiwes.demo.ds.ann.mgt.dao.IStudentDao;
 import com.digiwes.demo.ds.ann.mgt.dao.impl.StudentDao;
 import com.digiwes.demo.ds.ann.mgt.service.IStudentService;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
 
 import java.util.List;
 
 /**
  * Created by liurl3 on 2015/12/11.
  */
-/**
- * @scr.component name="demo.student.service" immediate=true
- * @scr.service value="com.digiwes.demo.ds.ann.mgt.service.IStudentService"
- */
+@Component (name = "demo.student.service",immediate = true)
+@Service (value = IStudentService.class)
 public class StudentService implements IStudentService {
     private IStudentDao studentDao = new StudentDao();
     @Override
